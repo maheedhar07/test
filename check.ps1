7317034192225615d5209df8f77719b440b4bd87
babu902 <127922325+babu902@users.noreply.github.com>
2023-04-08 09:29:08 +0530

    Update check.ps1

--- check.ps1
+++ check.ps1
@@ -1,49 +1,39 @@
-$OrganizationName = "organization"
-$ProjectName = "project"
-$sendmailto = "xxx@microsoft.com"
-$mysubject = "Test Mail Subjcet"
+# Define variables
+$emailSubject = "Test"
+$emailBody = "checking"
 
-$HeaderMail = @{
-    Authorization = "Bearer $env:SYSTEM_ACCESSTOKEN"
+$senderEmail = "EDGE-RBA@aib.ie"
+$recipientEmails = "kandan.x.munusamy@aib.ie", "abdulvahap.x.n@aib.ie", "EDGE_Wintel@aib.ie"
+$smtpServer = "smtp-ngwi-01.aib.pri"
+$reportPath = ""
+
+# Send email with report attachment
+try {
+    $message = New-Object System.Net.Mail.MailMessage
+    $message.From = $senderEmail
+    foreach ($recipient in $recipientEmails) {
+        $message.To.Add($recipient)
+    }
+    $message.Subject = $emailSubject
+    $message.Body = $emailBody
+    $message.IsBodyHtml = $true
+
+    $attachment = New-Object System.Net.Mail.Attachment($reportPath)
+    $message.Attachments.Add($attachment)
+
+    $smtp = New-Object System.Net.Mail.SmtpClient($smtpServer)
+    $smtp.Send($message)
+
+    Write-Output "Email sent successfully to $($recipientEmails -join ', ')"
 }
- 
-#Get the tfsid
-
-$userentitlementurl = "https://vsaex.dev.azure.com/${OrganizationName}/_apis/userentitlements?api-version=7.1-preview.1"
-
-$response = Invoke-RestMethod -Uri $userentitlementurl -Method Get -Headers $HeaderMail
-
-#Filter by sendmailto
-$tfsid = ($response.value| where {$_.user.mailAddress -eq $sendmailto}).id
-
-Write-Host $tfsid
-
-##send mail
-$urimail = "$env:SYSTEM_TEAMFOUNDATIONSERVERURI$env:SYSTEM_TEAMPROJECT/_apis/Release/sendmail/$($env:RELEASE_RELEASEID)?api-version=7.1-preview.1"
-
-$requestBody =
-@"
-{
-    "senderType": 1,
-    "to": {
-        "tfsIds": [
-            "$tfsid"
-        ],
-        "emailAddresses": []
-    },
-    "subject": "$mysubject",
-    "sections": [
-        5,
-        0,
-        1,
-        2,
-        4
-    ]
+catch {
+    Write-Error "Failed to send email: $($_.Exception.Message)"
 }
-"@
-Try {
-Invoke-RestMethod -Uri $urimail -Body $requestBody -Method POST -ContentType "application/json" -Headers $HeaderMail
-}
-Catch {
-$_.Exception
+finally {
+    if ($attachment) {
+        $attachment.Dispose()
+    }
+    if ($message) {
+        $message.Dispose()
+    }
 }
