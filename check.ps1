ea476875cb66681d7eb86979ec48b07473267496
babu902 <127922325+babu902@users.noreply.github.com>
2023-04-08 08:17:36 +0530

    Update check.ps1

--- check.ps1
+++ check.ps1
@@ -1,49 +1,69 @@
-# Define email parameters
-$from = "sender@example.com"
-$to = "recipient@example.com"
-$subject = "Test Execution Report - Failed Tests"
-$body = "The following tests have failed in the latest test execution: <br>"
-# Retrieve failed tests and append to the email body
+# Set input variables
+$myorg = "my-ado-org"
+$myproj = "my-ado-project"
+$sendmailto = "devops.user1@xyz.com,devops.user2@xyz.com" # comma separated email ids of receivers
+$mysubject = "my custom subject of the mail" # Subject of the email
+$mailbody = "my custom mail body details" # mail body
+$attachmentPath = "C:\path\to\folder\to\attach" # path to the folder to attach
 
-# Create a zip file of the folder to be attached
-$folderPath = "C:\path\to\folder" # Update with the path to your folder
-$zipPath = "C:\path\to\folder.zip" # Update with the desired path for the zip file
-Compress-Archive -Path $folderPath -DestinationPath $zipPath
+# Get TFS IDs of users to send mail
+$mailusers = "$sendmailto"
+$mymailusers = $mailusers -split ","
+$pat = "Bearer $env:System_AccessToken"
+$myurl ="https://dev.azure.com/${myorg}/_apis/projects/${myproj}/teams?api-version=5.1"
+$data = Invoke-RestMethod -Uri "$myurl" -Headers @{Authorization = $pat}
+$myteams = $data.value.id
 
-# Send email with folder attached
-Send-MailMessage -From $from -To $to -Subject $subject -Body $body -BodyAsHtml -SmtpServer "smtp.example.com" -Attachments $zipPath
+# Get list of members in all teams
+$myusersarray = @()
+foreach($myteam in $myteams) {
+    $usrurl = "https://dev.azure.com/${myorg}/_apis/projects/${myproj}/teams/"+$myteam+"/members?api-version=5.1"
+    $userdata = Invoke-RestMethod -Uri "$usrurl" -Headers @{Authorization = $pat}
+    $myusers = $userdata.value
+    foreach($myuser in $myusers) {
+        $myuserid = $myuser.identity.id
+        $myusermail = $myuser.identity.uniqueName
+        $myuserrecord = "$myuserid"+":"+"$myusermail"
+        $myusersarray += $myuserrecord
+    }
+}
 
+# Filter unique users and create hash of emails and TFS IDs
+$myfinalusersaray = $myusersarray | sort -Unique
+$myusershash = @{}
+for ($i = 0; $i -lt $myfinalusersaray.count; $i++)
+{
+    $myusershash[$myfinalusersaray[$i].split(":")[1]] = $myfinalusersaray[$i].split(":")[0]
+}
 
-  - task: PowerShell@2
-    displayName: 'Run PowerShell Script'
-    inputs:
-      targetType: 'filePath'
-      filePath: 'path/to/send_email.ps1' 
-      arguments: '-ExecutionPolicy Unrestricted'
-      
-      
-  
- steps:
-  - script: |
-      # Define email parameters
-      $from = "sender@example.com"
-      $to = "recipient@example.com"
-      $subject = "Test Execution Report - Failed Tests"
-      $body = "The following tests have failed in the latest test execution: <br>"
-      
-      # Retrieve failed tests and append to the email body
-      
-      # Create a zip file of the folder to be attached
-      $folderPath = "C:\path\to\folder" # Update with the path to your folder
-      $zipPath = "C:\path\to\folder.zip" # Update with the desired path for the zip file
-      Compress-Archive -Path $folderPath -DestinationPath $zipPath
-      
-      # Send email with folder attached
-      Send-MailMessage -From $from -To $to -Subject $subject -Body $body -BodyAsHtml -SmtpServer "smtp.example.com" -Attachments $zipPath
-    displayName: 'Execute PowerShell Script'
-  - task: PowerShell@2
-    inputs:
-      targetType: 'filePath'
-      filePath: '$(Agent.BuildDirectory)/path/to/your/powershell_script.ps1'
-      arguments: '-ExecutionPolicy Unrestricted'
-    displayName: 'Run PowerShell Script'
+# Create list of TFS IDs of mailers
+$myto = @()
+foreach($mymail in $mymailusers) {
+    $myto += $myusershash[$mymail]
+}
+
+# Convert attachment to base64
+$attachmentBytes = [System.IO.File]::ReadAllBytes($attachmentPath)
+$base64String = [System.Convert]::ToBase64String($attachmentBytes)
+$attachment = @{
+    'fileName' = [System.IO.Path]::GetFileName($attachmentPath)
+    'content' = $base64String
+    'contentType' = 'application/octet-stream'
+}
+
+# Send email
+$uri = "https://${myorg}.vsrm.visualstudio.com/${myproj}/_apis/Release/sendmail/$(RELEASE.RELEASEID)?api-version=3.2-preview.1"
+$requestBody = @{
+    "senderType"=1;
+    "to"=@{"tfsIds"=$myto};
+    "body"="$mailbody";
+    "subject"="$mysubject";
+    "attachments"=@($attachment)
+} | ConvertTo-Json
+
+try {
+    Invoke-RestMethod -Uri $uri -Body $requestBody -Method POST -Headers @{Authorization = $pat} -ContentType "application/json"
+}
+catch {
+    $_.Exception
+}
