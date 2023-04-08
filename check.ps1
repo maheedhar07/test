21f475c683d3562dbdf9ee2be321624fc268921c
babu902 <127922325+babu902@users.noreply.github.com>
2023-04-08 08:55:01 +0530

    Update check.ps1

--- check.ps1
+++ check.ps1
@@ -1,69 +1,49 @@
-# Set input variables
-$myorg = "my-ado-org"
-$myproj = "my-ado-project"
-$sendmailto = "devops.user1@xyz.com,devops.user2@xyz.com" # comma separated email ids of receivers
-$mysubject = "my custom subject of the mail" # Subject of the email
-$mailbody = "my custom mail body details" # mail body
-$attachmentPath = "C:\path\to\folder\to\attach" # path to the folder to attach
+$OrganizationName = "organization"
+$ProjectName = "project"
+$sendmailto = "xxx@microsoft.com"
+$mysubject = "Test Mail Subjcet"
 
-# Get TFS IDs of users to send mail
-$mailusers = "$sendmailto"
-$mymailusers = $mailusers -split ","
-$pat = "Bearer $env:System_AccessToken"
-$myurl ="https://dev.azure.com/${myorg}/_apis/projects/${myproj}/teams?api-version=5.1"
-$data = Invoke-RestMethod -Uri "$myurl" -Headers @{Authorization = $pat}
-$myteams = $data.value.id
-
-# Get list of members in all teams
-$myusersarray = @()
-foreach($myteam in $myteams) {
-    $usrurl = "https://dev.azure.com/${myorg}/_apis/projects/${myproj}/teams/"+$myteam+"/members?api-version=5.1"
-    $userdata = Invoke-RestMethod -Uri "$usrurl" -Headers @{Authorization = $pat}
-    $myusers = $userdata.value
-    foreach($myuser in $myusers) {
-        $myuserid = $myuser.identity.id
-        $myusermail = $myuser.identity.uniqueName
-        $myuserrecord = "$myuserid"+":"+"$myusermail"
-        $myusersarray += $myuserrecord
-    }
+$HeaderMail = @{
+    Authorization = "Bearer $env:SYSTEM_ACCESSTOKEN"
 }
+ 
+#Get the tfsid
 
-# Filter unique users and create hash of emails and TFS IDs
-$myfinalusersaray = $myusersarray | sort -Unique
-$myusershash = @{}
-for ($i = 0; $i -lt $myfinalusersaray.count; $i++)
+$userentitlementurl = "https://vsaex.dev.azure.com/${OrganizationName}/_apis/userentitlements?api-version=7.1-preview.1"
+
+$response = Invoke-RestMethod -Uri $userentitlementurl -Method Get -Headers $HeaderMail
+
+#Filter by sendmailto
+$tfsid = ($response.value| where {$_.user.mailAddress -eq $sendmailto}).id
+
+Write-Host $tfsid
+
+##send mail
+$urimail = "$env:SYSTEM_TEAMFOUNDATIONSERVERURI$env:SYSTEM_TEAMPROJECT/_apis/Release/sendmail/$($env:RELEASE_RELEASEID)?api-version=7.1-preview.1"
+
+$requestBody =
+@"
 {
-    $myusershash[$myfinalusersaray[$i].split(":")[1]] = $myfinalusersaray[$i].split(":")[0]
+    "senderType": 1,
+    "to": {
+        "tfsIds": [
+            "$tfsid"
+        ],
+        "emailAddresses": []
+    },
+    "subject": "$mysubject",
+    "sections": [
+        5,
+        0,
+        1,
+        2,
+        4
+    ]
 }
-
-# Create list of TFS IDs of mailers
-$myto = @()
-foreach($mymail in $mymailusers) {
-    $myto += $myusershash[$mymail]
+"@
+Try {
+Invoke-RestMethod -Uri $urimail -Body $requestBody -Method POST -ContentType "application/json" -Headers $HeaderMail
 }
-
-# Convert attachment to base64
-$attachmentBytes = [System.IO.File]::ReadAllBytes($attachmentPath)
-$base64String = [System.Convert]::ToBase64String($attachmentBytes)
-$attachment = @{
-    'fileName' = [System.IO.Path]::GetFileName($attachmentPath)
-    'content' = $base64String
-    'contentType' = 'application/octet-stream'
-}
-
-# Send email
-$uri = "https://${myorg}.vsrm.visualstudio.com/${myproj}/_apis/Release/sendmail/$(RELEASE.RELEASEID)?api-version=3.2-preview.1"
-$requestBody = @{
-    "senderType"=1;
-    "to"=@{"tfsIds"=$myto};
-    "body"="$mailbody";
-    "subject"="$mysubject";
-    "attachments"=@($attachment)
-} | ConvertTo-Json
-
-try {
-    Invoke-RestMethod -Uri $uri -Body $requestBody -Method POST -Headers @{Authorization = $pat} -ContentType "application/json"
-}
-catch {
-    $_.Exception
+Catch {
+$_.Exception
 }
